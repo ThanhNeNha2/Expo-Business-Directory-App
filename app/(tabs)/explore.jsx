a45bf9660f9e8b9f808959e41536b0bd347ac7bb
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-12-10 16:36:09 +0700

    first code

---
+++ app/(tabs)/explore.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Explore = () => {
+  return (
+    <View>
+      <Text>Explore Screen</Text>
+    </View>
+  );
+};
+
+export default Explore;
---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home Screen ne nha </Text>
+    </View>
+  );
+};
+
+export default Home;
